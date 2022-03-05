ae7df33e2397ddc6d7844394ccd5db8f7b5d248e
Slash Mod <90978423+SlashModProduct@users.noreply.github.com>
2022-03-05 23:58:10 +0100

    update

---
+++ shop/contacting.php
@@ -0,0 +1,10 @@
+<?php
+
+if($_POST["message"]) {
+
+mail("your@email.address", $name,
+
+$_POST["insert your message here"]. "From: an@email.address");
+}
+
+?>
---
+++ shop/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="SlashMod, Slash Mod, Logo, Developpement">
+		<meta name="description" content="Slash Mod est une entrepise de developpement informatique | Dev by Teramax and Valox">
+		<meta name="robots" content="index, follow, archive" />
+		<meta name="robots" content="all" />
+	
+	
+		<title>Slash Mod</title>
+	
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+	
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" />
+	
+		<!-- Owl Carousel -->
+		<link type="text/css" rel="stylesheet" href="../css/owl.carousel.css" />
+		<link type="text/css" rel="stylesheet" href="../css/owl.theme.default.css" />
+	
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	
+		<!-- Magnific Popup -->
+		<link type="text/css" rel="stylesheet" href="..//magnific-popup.css" />
+	
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="../css/font-awesome.min.css">
+	
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="../css/style.css" />
+	
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- favicon -->
+		<link rel="icon" type="image/png" href="../img/favicon.png" />
+	
+	</head>
+
+<body>
+
+	<!-- Header -->
+	<header>
+
+		<!-- Nav -->
+		<nav id="nav" class="navbar">
+			<div class="container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a href="../index.html">
+							<img class="logo" src="../img/logo.png" alt="logo">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Collapse nav button -->
+					<div class="nav-collapse">
+						<span></span>
+					</div>
+					<!-- /Collapse nav button -->
+				</div>
+
+				<!--  Main navigation  -->
+				<ul class="main-nav nav navbar-nav navbar-right">
+					<li><a href="../index.html#home">Home</a></li>
+					<li><a href="../index.html#about">A propos</a></li>
+					<li><a href="../index.html#portfolio">Projets</a></li>
+					<li><a href="../index.html#service">Compétences</a></li>
+					<li><a href="../index.html#features">Pourquoi nous ?</a></li>
+					<li><a href="../index.html#pricing">Nos prix</a></li>
+					<li><a href="../index.html#contact">Contact</a></li>
+				</ul>
+				<!-- /Main navigation -->
+
+			</div>
+		</nav>
+		<!-- /Nav -->
+
+		<!-- header wrapper -->
+		<div class="header-wrapper sm-padding bg-grey">
+			<div class="container">
+				<h2>Shop</h2>
+				<ul class="breadcrumb">
+					<li class="breadcrumb-item"><a href="../index.html">Home</a></li>
+					<li class="breadcrumb-item active">Shop</li>
+				</ul>
+			</div>
+		</div>
+		<!-- /header wrapper -->
+
+	</header>
+	<!-- /Header -->
+	
+	<div id="blog" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Main -->
+				<main id="main" class="col-md-9">
+					<div class="blog">
+						<div class="reply-form">
+							<h3 class="title">Nous contacter</h3>
+								<form action="contacting.php" method="post">
+									<input name="name" class="input" type="text" placeholder="Name" value="<?php echo $name; ?>">
+									<input name="email" class="input" type="email" placeholder="Email" value="<?php echo $name; ?>">
+									<textarea name="comment" placeholder="Add Your Commment" value="<?php echo $name; ?>"></textarea>
+									<button type="submit" class="main-btn">Envoyer</button>
+								</form>
+						</div>
+					<!-- /reply form -->
+					</div>
+				</main>
+				<!-- /Main -->
+			</div>
+			<!-- /Row -->
+		</div>
+		<!-- /Container -->
+	</div>
+	<!-- /Blog -->
+
+	<!-- Footer -->
+	<footer id="footer" class="sm-padding bg-dark">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<div class="col-md-12">
+
+					<!-- footer logo -->
+					<div class="footer-logo">
+						<a href="../index.html"><img src="../img/logo-alt.png" alt="logo"></a>
+					</div>
+					<!-- /footer logo -->
+
+					<!-- footer follow -->
+					<ul class="footer-follow">
+						<li><a href="https://discord.gg/qV9sFb5WQW" target="_blank"><i class="fab fa-discord"></i></a></li>
+						<li><a href="https://www.instagram.com/slashmodproduct/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="https://github.com/SlashModProduct" target="_blank"><i class="fa fa-github"></i></a></li>
+						<li><a href="https://www.youtube.com/channel/UCd3q5UBVAZW_C7AvPCq9vKg" target="_blank"><i class="fa fa-youtube"></i></a></li>
+					</ul>
+					<!-- /footer follow -->
+
+					<!-- footer copyright -->
+					<div class="footer-copyright">
+						<p>Copyright © 2021. All Rights Reserved. Designed by <a href="https://slashmod.com" target="_blank">Slash Mod</a></p>
+					</div>
+					<!-- /footer copyright -->
+
+				</div>
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</footer>
+	<!-- /Footer -->
+
+	<!-- Back to top -->
+	<div id="back-to-top"></div>
+	<!-- /Back to top -->
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="preloader">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<!-- /Preloader -->
+
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="../js/jquery.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="../js/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="../js/main.js"></script>
+
+</body>
+
+</html>
